ed5b452886e1d95198268df9450ac6ac74e9bf1d
vagrant <vagrant@precise64.(none)>
2015-02-21 23:57:43 +0000

    initial commit

---
+++ app/Http/Controllers/InstitutesController.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Support\Facades\Lang;
+
+class InstitutesController extends Controller {
+
+	public function create() {
+
+	}
+
+    public function update() {
+
+    }
+
+	public function index() {
+
+	}
+
+
+	public function store() {
+
+	}
+
+}
