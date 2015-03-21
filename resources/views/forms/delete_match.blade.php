a8daf67e9e59cb97941e36465e2273982a2872fa
ofir <ofirek1@gmail.com>
2015-03-21 13:48:28 +0200

    change

---
+++ resources/views/forms/delete_match.blade.php
@@ -0,0 +1,26 @@
+@extends('app')
+
+@section('page-title')
+    <h1>מחיקת שיבוץ קיים</h1>
+@stop
+
+@section('content')
+    <form class="psy-form" action="">
+
+
+
+                    <label>בחר פסיכולוג</label>
+                    <div class="input-line clearfix">
+                        <select name="type[]" class="pull-right mult" required>
+                            <option disabled="disabled" selected="selected" >בחר מרשימה</option>
+                            <option value="">דוד</option>
+                            <option value="">משה</option>
+                            <option value="">חיים</option>
+                        </select>
+                    </div>
+
+                 <h3>כאן תופיע טבלת כל השיבוצים של אותו פסיכולוג וניתן יהיה למחוק שיבוץ, כלומר שורה בטבלה</h3>
+
+
+    </form> <!-- /form -->
+@stop
