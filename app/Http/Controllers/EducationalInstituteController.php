e748116e5eb82a85e4dd23afdaead5b38144d931
ofir <ofirek1@gmail.com>
2015-04-04 11:07:24 +0300

    oo

--- app/Http/Controllers/EducationalInstituteController.php
+++ app/Http/Controllers/EducationalInstituteController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers;
 //ofir
 use App\Models\Psychologist;
 use App\Models\EducationalInstitute;
+use App\Models\Shapah;
 
 
 class EducationalInstituteController extends Controller {
@@ -17,6 +18,27 @@ class EducationalInstituteController extends Controller {
 	}
 
 
+	public function edit($educationalinstitutes_id) {
+		$educationalinstitute = educationalinstitute::find($educationalinstitutes_id);
+		$shapahs = Shapah::all();
+		return view('forms.institute_new', compact('educationalinstitute', 'shapahs'));
+	}
 
+
+	public function update($educationalinstitutes_id) {
+		$educationalinstitute = educationalinstitute::find($educationalinstitutes_id);
+		$input_data = Request;
+
+		$educationalinstitute->fill($input_data);
+		$educationalinstitute->save();
+
+		return redirect()->route('institute_page.index');
+	}
+
+
+    	public function show($educationalinstitutes_id) {
+		$educationalinstitute = educationalinstitute::find( $educationalinstitutes_id );
+		return view( 'singles.educational_institute', compact('educationalinstitute'));
+	}
 
 }
