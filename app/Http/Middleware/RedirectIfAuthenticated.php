7a7e9ef793b68a8a839310fb8a0a2c3ce33272d8
hagai <hagai.arad88@gmail.com>
2015-04-11 15:37:07 +0300

    hhhh

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -35,7 +35,7 @@ class RedirectIfAuthenticated {
 	{
 		if ($this->auth->check())
 		{
-			return new RedirectResponse(url('/psychologist'));
+			return new RedirectResponse(url('/map'));
 		}
 
 		return $next($request);
