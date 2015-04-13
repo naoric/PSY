a2180f1cbe12cc22ff2ed63597f84ede25c80bb2
ofir <ofirek1@gmail.com>
2015-04-14 01:10:42 +0300

    hhh

--- resources/views/forms/psy_new.blade.php
+++ resources/views/forms/psy_new.blade.php
@@ -36,15 +36,15 @@
 
             <span class="error">
 
-
-
-        </div>
-
             @if(isset($is_new) && !$is_new)
                 <label>הוסף שפ"ח</label>
                 <input tabindex="1" type="checkbox"  name="{{$add_shapah}}" id="{{$add_shapah}}">
 
             @endif
+
+        </div>
+
+
 
         <div class="input-line clearfix">
             <label>מועד תחילת עבודה</label>
